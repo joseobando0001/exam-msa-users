9c140ff7920961681f5cd66a6c12b09cc81176db
José Obando <jrobando0001@hotmail.com>
2025-04-06 13:00:41 -0500

    fix: added control of error detail

--- src/main/java/com/pichincha/exam/users/infrastructure/constants/ErrorConstants.java
+++ src/main/java/com/pichincha/exam/users/infrastructure/constants/ErrorConstants.java
@@ -6,4 +6,7 @@ public class ErrorConstants {
     }
 
     public static final String NOT_FOUND = "The client is not found";
+    public static final String DUPLICATED = "This client is duplicated";
+    public static final String ERROR_REQUEST = "The value is not valid";
+
 }
